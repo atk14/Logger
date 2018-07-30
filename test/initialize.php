caa5342ae54b405236f0e6d76cf986340ff8b287
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-07-30 19:53:49 +0200

    Sending email notification fixed; refactoring and testing

--- test/initialize.php
+++ test/initialize.php
@@ -1,12 +1,17 @@
 <?php
+define("TEST",true);
 define("LOGGER_DEFAULT_LOG_FILE",__DIR__."/log/default.log");
+define("SENDMAIL_DO_NOT_SEND_MAILS",true);
+
 require("../src/logger.php");
 require("../vendor/autoload.php");
+
 $LOGGER_CONFIGURATION = array(
 	"cache_remover" => array(
 		"log_file" => __DIR__."/log/cache_remover.log",
 	),
 	"import_*" => array(
+		"notify_email" => "import.notification@doe.com",
 		"log_file" => __DIR__."/log/import.log",
 	),
 );
